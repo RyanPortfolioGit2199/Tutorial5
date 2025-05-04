c7a8ebf6ad48eb4b70fcb6a93975a11b3e1a6d62
RyanPortfolioGit2199 <ryan.allen.2199@gmail.com>
2025-05-04 13:25:07 -0700

    5.2 complete

--- Assets/Course Library/Scripts/GameManger.cs
+++ Assets/Course Library/Scripts/GameManger.cs
@@ -15,7 +15,8 @@ public class GameManger : MonoBehaviour
     {
         StartCoroutine(SpawnTarget());
         score = 0;
-        scoreText.text = "Score: " +score;
+        UpdateScore(0);
+        
     }
 
     // Update is called once per frame
@@ -26,7 +27,7 @@ public class GameManger : MonoBehaviour
 
     IEnumerator SpawnTarget()
     {
-        while (true)/// A While Loop is a fusion of a For Loop and an If Statement, A While Loop will continually the code but instead of giving it a defined number of times(Like a For Loop). It uses a condition(True/False) to stop running (Like an If Statement)
+        while (true)/// A While Loop is a fusion of a For Loop and an If Statement, A While Loop will continually run the code but instead of giving it a defined number of times(Like a For Loop). It uses a condition(True/False) to stop running (Like an If Statement)
         {
             // Every 1 second(spawnRate) get a random number between 0 and the number of objects in the List, then spawn the object based on its number in the List.
             yield return new WaitForSeconds(spawnRate);
@@ -34,5 +35,11 @@ public class GameManger : MonoBehaviour
             Instantiate(targets[index]);
         }
 
+    }
+
+    public void UpdateScore(int scoreToAdd)// Need to make it public so I can reference this methoed outside of this script.
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
     }
 }
--- Assets/Course Library/Scripts/Target.cs
+++ Assets/Course Library/Scripts/Target.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class Target : MonoBehaviour
 {
@@ -8,12 +10,17 @@ public class Target : MonoBehaviour
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -6;
+    private GameManger gameManager;
+    public int pointValue;
+    public ParticleSystem explosionParticle;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         targetRb = GetComponent<Rigidbody>();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManger>();// Finds the Game Manager gameobject, then Gets the component GameManager Script to use that script references in this script.
+
 
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(),  ForceMode.Impulse); // AddTorque makes the object spin, applies the spin force instantly with Impulse.
@@ -30,11 +37,15 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        gameManager.UpdateScore(pointValue); //when destroy the targets Updates the score in the GameManger script based on the pointValue of each object destroyed.
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
+        
     }
 
     Vector3 RandomForce()
